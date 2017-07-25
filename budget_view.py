664c92425ce436bf745a036e970a4a5b80070563
Billy <billlyd123@gmail.com>
2017-07-25 16:00:55 -0700

    Edits on gui

--- budget_view.py
+++ budget_view.py
@@ -1,14 +1,31 @@
 import tkinter as tk
 
+
+class Selection_Menu():
+    def __init__(self, master):
+        self.canvas = tk.Canvas(
+            master = master, width = 600, height = 200,
+            background = 'white')
+
+        wefewf
+
+
+    def _select_add(self):
+        pass
+    
+
+
 class Budget_View():
     def __init__(self):
         self._root_window = tk.Tk()
-
-        self._selection_option = tk.Canvas(
-            master = self._root_window, width = 600, height = 200,
-            background = 'white')
+        self._selection = Selection_Menu(self._root_window)
         
-        self._selection_option.grid(row = 0, column = 0, padx = 10, pady = 10,
+        
+        self._selection.canvas.grid(row = 0, column = 0, padx = 10, pady = 10,
             sticky = tk.N + tk.S + tk.E + tk.W)
+
+
     
 Budget_View()
+
+
